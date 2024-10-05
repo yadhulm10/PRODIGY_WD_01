6324e7fbbb0a7b8417c3ea859ccae30a29f2a47a
Yadhu <yadhurajr10@gmail.com>
2024-10-05 20:28:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Change navbar background color on scroll
+window.addEventListener("scroll", function() {
+    var navbar = document.getElementById("navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
