fc781dc601eaef28857bb623d7c1c6a831bd0452
Yadhu Raj <yadhurajr10@gmail.com>
2024-10-12 16:23:51 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Change navbar background color on scroll
-window.addEventListener("scroll", function() {
-    var navbar = document.getElementById("navbar");
-    if (window.scrollY > 50) {
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
-    }
-});
